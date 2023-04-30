86c4b1c6a0e4356e7149aa964e4ab8a209e8fb66
Sharyn <70311132+sharyns@users.noreply.github.com>
2023-04-29 18:02:22 -0700

    Add files via upload

---
+++ Cash.java
@@ -0,0 +1,5 @@
+public class Cash implements Payment {
+	public void pay(double totalPaid) {
+		System.out.println(totalPaid + " paid with cash");
+	}
+}
---
+++ Kiosk.java
@@ -0,0 +1,7 @@
+public class Kiosk extends Shops {
+	public Kiosk() {
+		theOrder = new Ticket();
+		thePayment = new Cash();
+	}
+
+}
